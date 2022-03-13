78a045fe1a5ab2fe86d64b0c7e303fe8cc4127b4
StefanFischer123456789 <Stefan-Fischer98@web.de>
2022-03-13 16:43:53 +0100

    jar signed

--- src/main/java/teil2/task01/PortConfiguration.java
+++ src/main/java/teil2/task01/PortConfiguration.java
@@ -3,7 +3,7 @@ package teil2.task01;
 public enum PortConfiguration {
     instance;
     public final String nameOfJarFolder = "Component";
-    public final String nameOfJar = "MixingComponent";
+    public final String nameOfJar = "signedjar";
     public final String userDirectory = System.getProperty("user.dir");
     public final String fileSeparator = System.getProperty("file.separator");
     public final String pathToComponent = userDirectory + fileSeparator + "src" + fileSeparator + "main" + fileSeparator + "java"+fileSeparator + "teil2"+fileSeparator + "task01";
